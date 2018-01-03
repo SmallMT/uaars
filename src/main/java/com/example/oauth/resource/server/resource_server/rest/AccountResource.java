b7eaf5201c704094515614f4a49f319507fd8765
刘亚涛 <739697044@qq.com>
2018-01-03 11:28:29 +0800

    邮箱可重复

--- src/main/java/com/example/oauth/resource/server/resource_server/rest/AccountResource.java
+++ src/main/java/com/example/oauth/resource/server/resource_server/rest/AccountResource.java
@@ -92,9 +92,9 @@ public class AccountResource {
         userRepository.findOneByLogin(managedUserVM.getLogin().toLowerCase()).ifPresent(u -> {
             throw new LoginAlreadyUsedException();
         });
-        userRepository.findOneByEmail(managedUserVM.getEmail()).ifPresent(u -> {
-            throw new EmailAlreadyUsedException();
-        });
+//        userRepository.findOneByEmail(managedUserVM.getEmail()).ifPresent(u -> {
+//            throw new EmailAlreadyUsedException();
+//        });
         User user = userService.registerUser(managedUserVM, managedUserVM.getPassword());
         mailService.sendActivationEmail(user);
     }
